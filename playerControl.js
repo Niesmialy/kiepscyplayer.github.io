d65f33006bf767033322aee3523e42e4270c9f8a
kiepscyplayer <118777361+kiepscyplayer@users.noreply.github.com>
2022-11-22 12:58:07 +0100

    mobile support!!!

--- playerControl.js
+++ playerControl.js
@@ -2,6 +2,9 @@ const player = document.getElementById('player')
 
 function playEp(url) {
     player.src = url
+    player.controls = true
+    player.loop = false
+    player.muted = false
     player.load()
     player.play()
 }
